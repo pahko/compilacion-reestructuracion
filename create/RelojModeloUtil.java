5aa6f300cbfbbefca7a5cee43ff9f996ff3c8759
Gerardo Orozco <orozcomgera@gmail.com>
2011-11-09 23:16:21 -0600

    mas clases

--- create/RelojModeloUtil.java
+++ create/RelojModeloUtil.java
@@ -1,4 +1,4 @@
-// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 07/11/2011 05:22:28 p.m.
+// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov
 // Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
 // Decompiler options: packimports(3) 
 // Source File Name:   RelojModeloUtil.java
@@ -8,46 +8,28 @@ package create;
 import java.io.PrintStream;
 import java.util.*;
 
-public class RelojModeloUtil extends Observable
-{
+public class RelojModeloUtil extends Observable {
 
-    public RelojModeloUtil()
-    {
+    public RelojModeloUtil() {
         timerTask = new TimerTask() {
 
-            public void run()
-            {
+            public void run() {
                 setChanged();
                 notifyObservers(new Date());
             }
-
-            final RelojModeloUtil this$0;
-
-            
-            {
-                this$0 = RelojModeloUtil.this;
-                super();
-            }
-        }
-;
+        };
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(timerTask, 0L, 1000L);
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         RelojModeloUtil modelo = new RelojModeloUtil();
         modelo.addObserver(new Observer() {
 
-            public void update(Observable unObservable, Object dato)
-            {
+            public void update(Observable unObservable, Object dato) {
                 System.out.println(dato);
             }
-
-        }
-);
+        });
     }
-
     TimerTask timerTask;
-
 }
--- create/ServidorChat.java
+++ create/ServidorChat.java
@@ -1,4 +1,4 @@
-// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 07/11/2011 05:26:58 p.m.
+// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov
 // Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
 // Decompiler options: packimports(3) 
 // Source File Name:   ServidorChat.java
@@ -12,34 +12,25 @@ import javax.swing.DefaultListModel;
 // Referenced classes of package create:
 //            HiloDeCliente
 
-public class ServidorChat
-    implements Runnable
-{
+public class ServidorChat implements Runnable {
 
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             ServidorChat_Inicio();
         }
-        catch(Exception ex)
-        {
+        catch(Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public ServidorChat()
-    {
+    public ServidorChat() {
         charla = new DefaultListModel();
     }
 
-    public void ServidorChat_Inicio()
-    {
-        try
-        {
+    public void ServidorChat_Inicio() {
+        try {
             ServerSocket socketServidor = new ServerSocket(5557);
-            do
-            {
+            do {
                 System.out.println("DENTRO DEL SERVIDOR!!");
                 java.net.Socket cliente = socketServidor.accept();
                 Runnable nuevoCliente = new HiloDeCliente(charla, cliente);
@@ -47,12 +38,10 @@ public class ServidorChat
                 hilo.start();
             } while(true);
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             e.printStackTrace();
         }
         System.exit(1);
     }
-
     private DefaultListModel charla;
 }
