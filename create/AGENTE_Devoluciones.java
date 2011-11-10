d9bcc3cf6291bf162f473580dbfd5e434aca9985
Fernando Hernandez <rasoner@gmail.com>
2011-11-09 22:41:15 -0600

    mas arreglado

--- create/AGENTE_Devoluciones.java
+++ create/AGENTE_Devoluciones.java
@@ -1,4 +1,4 @@
-// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 08/11/2011 05:38:45 p.m.
+// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshko
 // Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
 // Decompiler options: packimports(3) 
 // Source File Name:   AGENTE_Devoluciones.java
