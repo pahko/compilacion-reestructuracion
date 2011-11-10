2498a0650749bed3e2a7379f5eae185e65f63f19
Fernando Hernandez <rasoner@gmail.com>
2011-11-09 22:29:17 -0600

    agentes terminados

--- create/AGENTE_Devoluciones.java
+++ create/AGENTE_Devoluciones.java
@@ -19,17 +19,15 @@ import javax.swing.border.SoftBevelBorder;
 
 public class AGENTE_Devoluciones extends JFrame
 {
+	
     public class RelojModeloUtil extends Observable
     {
 
         TimerTask timerTask;
-        final AGENTE_Devoluciones this$0;
-
 
         public RelojModeloUtil()
         {
         	super();
-            this$0 = AGENTE_Devoluciones.this;
             timerTask = new TimerTask() {
 
                 public void run()
@@ -37,16 +35,8 @@ public class AGENTE_Devoluciones extends JFrame
                     setChanged();
                     notifyObservers(new Date());
                 }
-
-                final RelojModeloUtil this$1;
-
-                
-                {
-                	//super();
-                    this$1 = RelojModeloUtil.this;
-                }
-            }
-;
+            };
+            
             Timer timer = new Timer();
             timer.schedule(timerTask, 0L, 1000L);
         }
@@ -114,17 +104,7 @@ public class AGENTE_Devoluciones extends JFrame
                     seg++;
                 }
             }
-
-            private Fosar_Agente fosa;
-            final AGENTE_Devoluciones this$0;
-
-            
-            {
-                this$0 = AGENTE_Devoluciones.this;
-                //super();
-            }
-        }
-);
+        });
     }
 
     private void initComponents()
@@ -155,10 +135,9 @@ public class AGENTE_Devoluciones extends JFrame
                 (new AGENTE_Devoluciones()).setVisible(true);
             }
 
-        }
-);
+        });
     }
-
+    
     int SEGUNDOS;
     int seg;
     boolean FACTURANDO;
@@ -167,78 +146,4 @@ public class AGENTE_Devoluciones extends JFrame
 
 }
 
-class AGENTE_Devoluciones$1 implements Observer
-	{
-	
-	public void update(Observable unObservable, Object dato)
-	{
-	    this$0.Reloj.setText(dato.toString());
-	    System.out.println(dato);
-	    System.out.println((new StringBuilder()).append("SEG : ").append(seg).toString());
-	    if(FACTURA && (seg == 0 || seg == 10 || seg == 20 || seg == 30 || seg == 40 || seg == 50))
-	        try
-	        {
-	            if(!FACTURANDO)
-	            {
-	                FACTURANDO = true;
-	                Fosar_Agente_Devoluciones fosa;
-	                try
-	                {
-	                    fosa = new Fosar_Agente_Devoluciones();
-	                }
-	                catch(Exception ex)
-	                {
-	                    ex.printStackTrace();
-	                }
-	                FACTURANDO = false;
-	            }
-	            System.gc();
-	        }
-	        catch(Exception ex)
-	        {
-	            ex.printStackTrace();
-	        }
-	    if(seg == 0)
-	    {
-	        FACTURA = false;
-	        System.out.println("BORRANDO ....");
-	        System.out.println("Datos Borrados !!!");
-	        System.out.println("Copiando ..... ");
-	        System.out.println("Copia Finalizada !!!");
-	        FACTURA = true;
-	    }
-	    if(seg >= 60)
-	    {
-	        seg = 0;
-	        System.exit(0);
-	    } else
-	    {
-	        seg++;
-	    }
-	}
-	
-	private Fosar_Agente fosa;
-	final AGENTE_Devoluciones this$0;
-	int SEGUNDOS;
-	int seg;
-	boolean FACTURANDO;
-	boolean FACTURA;
-	
-	public AGENTE_Devoluciones$1()
-	{
-	    super();
-	    this$0 = new AGENTE_Devoluciones();
-	}
-}
 
-class AGENTE_Devoluciones$2 implements Runnable 
-{
-    public void run()
-    {
-        (new AGENTE_Devoluciones()).setVisible(true);
-    }
-
-    AGENTE_Devoluciones$2()
-    {
-    }
-}
