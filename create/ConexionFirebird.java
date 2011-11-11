e44c2c3526579b7a86fea307ba891178461cbcfa
pahko <pahko.xd@gmail.com>
2011-11-10 20:13:33 -0600

    ConexionFirebird

--- create/ConexionFirebird.java
+++ create/ConexionFirebird.java
@@ -1,16 +1,9 @@
-// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 07/11/2011 04:56:40 p.m.
-// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
-// Decompiler options: packimports(3) 
-// Source File Name:   ConexionFirebird.java
-
 package create;
 
 import java.io.*;
 import java.sql.*;
 import java.util.Properties;
 
-// Referenced classes of package create:
-//            Mensajes, ProgressBarSample
 
 public class ConexionFirebird {
 	
@@ -31,13 +24,16 @@ public class ConexionFirebird {
 
     public static void conectarFirebird() throws Exception {
     	try {
-	        System.out.println("Ruta de instalacion de Base Facture FB..C:\\FDE\\ ");
-	        System.out.println("Leyendo archivo de propiedades en ruta princial  C:\\FDE\\");
+	        System.out.println("Ruta de instalacion de Base Facture " +
+	        		"FB..C:\\FDE\\ ");
+	        System.out.println("Leyendo archivo de propiedades en ruta " +
+	        		"princial  C:\\FDE\\");
 	        fproperties = new FileInputStream("C:\\FDE\\firebird.properties");
 	        p.load(fproperties);
 	        maquina = p.getProperty("firebird.drda.host");
-	        if(maquina.equals("0.0.0.0"))
-	            maquina = "localhost";
+	        if (maquina.equals("0.0.0.0")) {
+	        	maquina = "localhost";
+	        }
 	        port = p.getProperty("firebird.drda.portNumber");
 	        ruta = p.getProperty("firebird.drda.path");
 	        System.out.println("Cerrando Archivo en C:\\FDE\\ .......");
@@ -45,161 +41,116 @@ public class ConexionFirebird {
 	        System.out.println("Archivo en C:\\FDE\\  CERRADO !!!!");
     	} catch(Exception ex) {
     		ex.printStackTrace();
-    	}        
+    	}   
+    	
         try {
-            String databaseURL = (new StringBuilder()).append("jdbc:firebirdsql:").append(maquina).append("/").append(port).append(":").append(ruta).toString();
-            System.out.println((new StringBuilder("Ejecutando de conexion..[ ")).append(databaseURL).append(" ]").toString());
+            String databaseURL = (new StringBuilder())
+            	.append("jdbc:firebirdsql:").append(maquina).append("/")
+            	.append(port).append(":").append(ruta).toString();
+            System.out.println((new StringBuilder("Ejecutando de conexion..[ "))
+            		.append(databaseURL).append(" ]").toString());
             String user = "SYSDBA";
             String password = "masterkey";
             String driverName = "org.firebirdsql.jdbc.FBDriver";
             Class.forName(driverName);
             if(!databaseURL.equals(null)) {
-                boolean centinela = true;
                 try {
-                    con = DriverManager.getConnection(databaseURL, user, password);
+                    con = DriverManager.getConnection(databaseURL, user, 
+                    								  password);
                     con.setAutoCommit(false);
-                    centinela = false;
                 } catch(SQLException e) {
-                    System.out.println((new StringBuilder("Servicio FireBird No Activo en ruta\n")).append(databaseURL).append("\nContacte A Su Proveedor").toString());
-                    m.GetMensaje("CONEXION A SERVIDOR DE FACTURACION ERRONEA, FAVOR DE VERIFICAR SU RED");
+                    System.out.println((new StringBuilder("Servicio FireBird " +
+                    		"No Activo en ruta\n")).append(databaseURL)
+                    		.append("\nContacte A Su Proveedor").toString());
+                    m.GetMensaje("CONEXION A SERVIDOR DE FACTURACION " +
+                    		"ERRONEA, FAVOR DE VERIFICAR SU RED");
                     System.exit(1);
                 }
-            } else
-            {
-                m.GetMensaje("CONEXION A SERVIDOR DE FACTURACION ERRONEA, FAVOR DE VERIFICAR SU RED");
+            } else {
+                m.GetMensaje("CONEXION A SERVIDOR DE FACTURACION ERRONEA, " +
+                		"FAVOR DE VERIFICAR SU RED");
                 System.exit(1);
             }
         }
         // Misplaced declaration of an exception variable
-        catch(Exception e)
-        {
-            m.GetMensaje("CONEXION A SERVIDOR DE FACTURACION ERRONEA, FAVOR DE VERIFICAR SU RED");
+        catch(Exception e) {
+            m.GetMensaje("CONEXION A SERVIDOR DE FACTURACION ERRONEA, FAVOR " +
+            		"DE VERIFICAR SU RED");
             System.exit(1);
         }
         return;
     }
 
     public ResultSet consulta(String qry, boolean result)
-        throws SQLException
-    {
+        throws SQLException {
         stmt = con.createStatement(1004, 1008);
-        if(result)
-            rs = stmt.executeQuery(qry);
-        else
-            stmt.execute(qry);
+        if(result) {
+        	rs = stmt.executeQuery(qry);
+        } else {
+        	stmt.execute(qry);
+        }
         return rs;
     }
 
-    public ResultSet consultaBLOB(String qry, boolean result)
-        throws SQLException
-    {
+    public ResultSet consultaBLOB(String qry, boolean result) 
+    		throws SQLException {
         stmt = con.createStatement();
-        if(result)
+        if (result)
             rs = stmt.executeQuery(qry);
         else
             stmt.execute(qry);
         return rs;
     }
 
-    public void prepara_consulta(String qry)
-    {
-        try
-        {
+    public void prepara_consulta(String qry) {
+        try {
             pst = con.prepareStatement(qry);
-        }
-        catch(SQLException e)
-        {
+        } catch(SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void aplicar_prepara_consulta()
-    {
-        try
-        {
+    public void aplicar_prepara_consulta() {
+        try {
             pst.executeUpdate();
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-        try
-        {
             pst.close();
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-        try
-        {
             con.commit();
-        }
-        catch(SQLException e)
-        {
+        } catch(SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void consultaUpdate(String qry, boolean result)
-        throws SQLException
-    {
+    public void consultaUpdate(String qry, boolean result) throws SQLException {
         stmt = con.createStatement();
         stmt.executeUpdate(qry);
     }
 
-    public void libera(ResultSet rs)
-        throws SQLException
-    {
-        if(rs != null)
-            rs.close();
-        if(rs != null)
-            rs.close();
-        if(stmt != null)
-            stmt.close();
+    public void libera(ResultSet rs) throws SQLException {
+        if (rs != null) rs.close();
+        if (stmt != null) stmt.close();
         rs = null;
         stmt = null;
     }
 
-    public void commit()
-        throws SQLException
-    {
+    public void commit() throws SQLException {
         con.commit();
     }
 
-    public void finalizar()
-        throws SQLException
-    {
-        try
-        {
+    public void finalizar() throws SQLException {
+        try {
             con.close();
-        }
-        catch(SQLException e)
-        {
+        } catch(SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public String rollback()
-    {
-        try
-        {
-            con.rollback();
-        }
-        catch(SQLException e)
-        {
+    public String rollback() {
+        try {
+        	con.rollback();
+        } catch(SQLException e) {
             return e.getMessage();
         }
         return "";
-    }
-
-    public static void main(String args[])
-        throws FileNotFoundException, ClassNotFoundException, SQLException, IOException
-    {
-        ConexionFirebird cnx = new ConexionFirebird();
-        ConexionFirebird _tmp = cnx;
-        conectarFirebird();
-        cnx.finalizar();
     }
 
 }
