89b6e72203bee95c869789af21f5e6e5fc3d2937
pahko <pahko.xd@gmail.com>
2011-11-10 19:50:21 -0600

    Test sin comentarios

--- create/Test.java
+++ create/Test.java
@@ -1,13 +1,6 @@
-// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 07/11/2011 06:08:47 p.m.
-// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
-// Decompiler options: packimports(3) 
-// Source File Name:   Test.java
-
 package create;
 
-import java.awt.HeadlessException;
 import java.io.*;
-import java.sql.SQLException;
 
 // Referenced classes of package create:
 //            Comprobantebd, ConexionFirebird
@@ -22,7 +15,6 @@ public class Test {
         Comprobantebd cbd = new Comprobantebd();
         ConexionFirebird cnx = new ConexionFirebird();
         try {
-            ConexionFirebird _tmp = cnx;
             ConexionFirebird.conectarFirebird();
             cbd.estableceConexion(cnx);
             cbd.reporteMensual("MAYO2010.txt", "2010-05-01", "2010-05-31");
