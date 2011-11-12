550d5f620de1908e1081b2000d3b756cd2222ce6
Noel Meza <nelmeza17@gmail.com>
2011-11-11 21:16:00 -0600

    Docs to AppcfdComprobanteExcecute

--- src/comprobante/AppcfdComprobanteExecute.java
+++ src/comprobante/AppcfdComprobanteExecute.java
@@ -1,4 +1,3 @@
-// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 07/11/2011 03:56:55 p.m.
 // Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
 // Decompiler options: packimports(3) 
 // Source File Name:   AppcfdComprobanteExecute.java
@@ -15,7 +14,7 @@ import org.jdom.JDOMException;
 //            AppcfdComprobante
 
 public class AppcfdComprobanteExecute {
-
+	/**Main de la Clase AppcfdComprobableExcecute*/
     public static void main(String arg[]) throws Exception {
         apc.conected();
         try
